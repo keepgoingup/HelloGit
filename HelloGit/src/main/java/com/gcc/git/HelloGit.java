d1df383cb808e2277133aba2ea0fc2ce2aa4ae5e
chenchao <guochenchao@foxmail.com>
2021-04-01 22:03:22 +0800

    commit file v1

---
+++ HelloGit/src/main/java/com/gcc/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.gcc.git;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 BNC, Inc. All rights reserved. <p>
+ * Company: 北京巨网云互联科技有限公司<p>
+ *
+ * @author Chenchao Guo
+ * @since 2021/4/1 12:50
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
